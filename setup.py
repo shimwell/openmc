ed29e461ed389e0c37253623e0c83bbc58a086c6
Jonathan Shimwell <mail@jshimwell.com>
2022-10-07 16:58:03 +0100

    min diff

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ if sys.platform == 'darwin':
 else:
     suffix = 'so'
 
-# Get version information from __init__.py. This is ugly, but more reliable
-# than using an import.
+# Get version information from __init__.py. This is ugly, but more reliable than
+# using an import.
 with open('openmc/__init__.py', 'r') as f:
     version = f.readlines()[-1].split()[-1].strip("'")
 
@@ -30,14 +30,14 @@ kwargs = {
     # Data files and libraries
     'package_data': {
         'openmc.lib': ['libopenmc.{}'.format(suffix)],
-        'openmc.data': ['*.pyx', '*.c', 'mass16.txt', 'BREMX.DAT', 'half_life.json', '*.h5'],
+        'openmc.data': ['mass16.txt', 'BREMX.DAT', 'half_life.json', '*.h5'],
         'openmc.data.effective_dose': ['*.txt']
     },
     'python_requires': '>=3.7',
     'include_package_data': True,
     'install_requires': [
         'numpy>=1.9', 'h5py', 'scipy', 'ipython', 'matplotlib',
-        'pandas', 'lxml', 'uncertainties', 'cython'
+        'pandas', 'lxml', 'uncertainties'
     ],
     'ext_modules': cythonize('openmc/data/*.pyx'),
     'include_dirs': [np.get_include()]
