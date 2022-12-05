26f9782499297d143249a76784721d44eb2a5943
shimwell <mail@jshimwell.com>
2022-12-05 20:45:16 +0000

    removed is filled check

--- openmc/model/model.py
+++ openmc/model/model.py
@@ -976,8 +976,6 @@ class R2SModel(Model):
             # Collect all spatial distributions of the sources from the model
             box_map = {}
             for cell in self.geometry.get_all_material_cells().values():
-                if cell.fill is None:
-                    continue
                 lower_left, upper_right = cell.region.bounding_box
                 box = openmc.stats.Box(lower_left, upper_right)
                 box_map[cell.fill.id] = box
