f21675c8968597ec5e72b84aeefb5c0368db06b3
Jon Shimwell <jon@proximafusion.com>
2024-07-10 21:51:41 +0200

    added deps package

---
+++ cibw_before_build.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+set -eu
+
+# build script for manylinux-x86_64-image= "manylinux_2_28"
+# install openmc with dagmc embree and double down
+# packages are compiled in this install script using all available CPU cores.
+# to reduce the core usage to 2 replace -j commands with -j2
+
+dnf install -y epel-release
+dnf config-manager --enable epel
+
+# dnf search netcdf
+
+dnf install -y sudo curl cmake eigen3-devel gcc gcc-c++ wget \
+               hdf5-devel mpich mpich-devel netcdf-devel netcdf-mpich-devel \
+               metis python3 python3-devel python3-pip
+pip install cython
