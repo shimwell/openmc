37f22ccfac4dcbe5670c75d45625632bf741399e
Jonathan Shimwell <mail@jshimwell.com>
2024-08-27 23:24:06 +0100

    [skip ci] returned to previous state as error not fixed

--- setup.py
+++ setup.py
@@ -10,22 +10,10 @@ class OpenMCExtension(Extension):
         Extension.__init__(self, name, sources=sources, **kwa)
         self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
 
-lib_dir = os.path.join('openmc', 'lib')
-libopenmc_path = os.path.join(lib_dir, 'libopenmc.so')
-
-# Ensure the library exists
-if not os.path.exists(libopenmc_path):
-    raise FileNotFoundError(f"Required library {libopenmc_path} not found")
 
 kwargs = {
     'ext_modules': [OpenMCExtension('libopenmc')],
-    'include_dirs': [np.get_include()],
-    'package_data': {
-        'openmc.lib': ['libopenmc.so'],
-    },
-    'data_files': [
-        (lib_dir, [libopenmc_path]),
-    ],
+    'include_dirs': [np.get_include()]
 }
 
 setup(**kwargs)
