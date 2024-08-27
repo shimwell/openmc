22df26a99adce7d436a3c349409cacd54151f9fe
Jonathan Shimwell <mail@jshimwell.com>
2024-08-27 22:43:05 +0100

    trying to get libopenmc.so found

--- setup.py
+++ setup.py
@@ -10,10 +10,15 @@ class OpenMCExtension(Extension):
         Extension.__init__(self, name, sources=sources, **kwa)
         self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
 
+lib_dir = os.path.join('openmc', 'lib')
+libopenmc_path = os.path.join(lib_dir, 'libopenmc.so')
 
 kwargs = {
     'ext_modules': [OpenMCExtension('libopenmc')],
-    'include_dirs': [np.get_include()]
+    'include_dirs': [np.get_include()],
+    'package_data': {
+        'openmc': [os.path.join('lib', 'libopenmc.so')],
+    },
 }
 
 setup(**kwargs)
