d85fd1f713aaee93b983d537451b083dbe235c5c
Patrick Shriwise <pshriwise@gmail.com>
2026-02-04 14:05:53 -0600

    Accounting for XDGMesh when creating MeshFilters

--- openmc/filter.py
+++ openmc/filter.py
@@ -925,6 +925,8 @@ class MeshFilter(Filter):
                 self.bins = list(range(len(mesh.volumes)))
             else:
                 self.bins = []
+        elif isinstance(mesh, openmc.XDGMesh):
+            self.bins = []
         else:
             self.bins = list(mesh.indices)
 
